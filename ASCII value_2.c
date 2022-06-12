31e0ffe468e041004feb444bfae2ae0daac61380
sb shuvon <sbshuvon07@gmail.com>
2022-06-12 07:42:39 -0600

    Create ASCII value_2.c

---
+++ ASCII value_2.c
@@ -0,0 +1,16 @@
+
+// Character to ASCII value
+
+#include<stdio.h>
+int main()
+{
+    char ch;
+
+    printf("Enter any character: ");
+    scanf("%c",&ch);
+
+    printf("The ASCII Value is: %d",ch);
+
+
+    getch();
+}
