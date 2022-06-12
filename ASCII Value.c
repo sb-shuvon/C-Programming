44fb08a531e689ddb47bd9e8380adfbf3a7b9996
sb shuvon <sbshuvon07@gmail.com>
2022-06-12 07:32:42 -0600

    Create ASCII Value.c

---
+++ ASCII Value.c
@@ -0,0 +1,17 @@
+
+//ASCII Value to ASCII character
+
+#include<stdio.h>
+int main()
+{
+    int n;
+
+    printf("Enter any ASCII value: ");
+    scanf("%d",&n);
+
+    printf("The ASCII character is: %c",n);
+
+
+
+    getch();
+}
