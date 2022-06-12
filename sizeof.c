d94f85179352993791acafd8812c4d529abd5f6d
sb shuvon <sbshuvon07@gmail.com>
2022-06-12 07:19:56 -0600

    Create sizeof.c

---
+++ sizeof.c
@@ -0,0 +1,18 @@
+//size of value
+
+#include<stdio.h>
+int main()
+{
+    int i;
+    float f;
+    double d;
+    char c;
+
+    printf("size of int= %d\n",sizeof(i));
+    printf("size of float= %d\n",sizeof(f));
+    printf("size of double= %d\n",sizeof(d));
+    printf("size of char= %d\n",sizeof(c));
+
+
+    getch();
+}
