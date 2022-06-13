e74c7716dcdbe329e0fa4b197f1d9637d7fca201
sb shuvon <sbshuvon07@gmail.com>
2022-06-13 04:32:17 -0600

    Create Triangle_3 values.c

---
+++ Triangle_3 values.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,c,s,area;
+
+    printf("Enter 3 value of Triangle: ");
+    scanf("%f %f %f",&a,&b,&c);
+
+    s = a+b+c/2;
+    area = sqrt ((s-a)*(s-b)*(s-c));
+
+    printf("Triangle area are: %.2f\n",area);
+
+
+
+    getch();
+}
