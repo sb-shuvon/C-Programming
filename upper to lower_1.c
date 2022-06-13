fa947980135068863ec8a7f074d01932109cb77f
sb shuvon <sbshuvon07@gmail.com>
2022-06-13 00:42:28 -0600

    Create upper to lower_1.c

---
+++ upper to lower_1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char upper;
+    printf("Enter any Uppercase latter: ");
+    scanf("%c",&upper);
+
+    printf("The lowercase letter is : %c",upper+32);
+
+
+    getch();
+}
