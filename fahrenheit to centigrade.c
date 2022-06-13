fd3334e963aedc24cba421d88527313784f6d9fe
sb shuvon <sbshuvon07@gmail.com>
2022-06-13 12:39:28 -0600

    Update fahrenheit to centigrade.c

--- fahrenheit to centigrade.c
+++ fahrenheit to centigrade.c
@@ -1,1 +1,19 @@
 
+
+//fahrenheit to centigrade  calculate
+
+#include<stdio.h>
+int main()
+{
+    float f,c;
+
+    printf("Enter Fahrenheit: ");
+    scanf("%f",&f);
+
+    c = (f-32)/1.8;
+    printf("Centigrade are= %f",c);
+
+
+
+    getch();
+}
