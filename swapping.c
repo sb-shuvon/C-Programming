3b891aba76d60743989ed0a55a48b631450a05b1
sb shuvon <sbshuvon07@gmail.com>
2022-06-13 12:47:29 -0600

    Create swapping.c

---
+++ swapping.c
@@ -0,0 +1,26 @@
+
+//swapping two numbers
+
+#include<stdio.h>
+int main()
+{
+    int num1,num2,temp;
+
+    num1=10;
+    num2=20;
+
+    printf("After Swapping\n");
+    printf("num1 = %d\n",num1);
+    printf("num2 = %d\n\n",num2);
+
+    temp= num1;
+    num1= num2;
+    num2= temp;
+
+    printf("Before Swapping\n");
+    printf("num1 = %d\n",num1);
+    printf("num2 = %d\n",num2);
+
+
+    getch();
+}
