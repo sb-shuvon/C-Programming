bea08bf701ab4a4447cf71b3789ff730e667643b
sb shuvon <sbshuvon07@gmail.com>
2022-06-13 00:42:44 -0600

    Create octal to decimal.c

---
+++ octal to decimal.c
@@ -0,0 +1,14 @@
+//convert between Octal number to Decimal number
+
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("Enter Octal number: ");
+    scanf("%o",&number);
+
+    printf("Decimal number= %d",number);
+
+
+    getch();
+}
