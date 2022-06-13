c71f0a016d7cbbaeb34a44e849988f9965649f7d
sb shuvon <sbshuvon07@gmail.com>
2022-06-13 00:43:27 -0600

    Create hexa-decimal to decimal.c

---
+++ hexa-decimal to decimal.c
@@ -0,0 +1,16 @@
+// convert between hexa-decimal number to decimal number
+
+
+#include<stdio.h>
+int main()
+{
+    int number;
+
+    printf("Enter Hexa-decimal number: ");
+    scanf("%x",&number);
+
+    printf("Decimal nmber= %d",number);
+
+
+    getch();
+}
