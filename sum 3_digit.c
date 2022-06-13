f19121224880c403a7754fbf4a69bc413e923ec0
sb shuvon <sbshuvon07@gmail.com>
2022-06-13 04:15:28 -0600

    Create sum 3_digit.c

---
+++ sum 3_digit.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,num3,sum;
+    float avg;
+
+    printf("Enter 3 numbers: ");
+    scanf("%d %d %d",&num1,&num2,&num3);
+
+    sum = num1+ num2 + num3;
+    avg = (float)sum/3;
+
+    printf("Sum= %d\n",sum);
+    printf("Avarage= %.2f\n",avg);
+
+
+    getch();
+}
