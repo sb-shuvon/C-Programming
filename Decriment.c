edf5144a8f382e6008d183b5e40087a19260a073
sb shuvon <sbshuvon07@gmail.com>
2022-06-14 00:21:07 -0600

    Create Decriment.c

---
+++ Decriment.c
@@ -0,0 +1,30 @@
+
+
+/*
+    Increment
+        1. Prefix Increment
+        2. Postfix Increment
+*/
+
+#include<stdio.h>
+int main()
+{
+    int x=10;
+
+    int y = x--;
+    printf("x = %d\n",x);
+    printf("y = %d\n",y);
+    printf("\n\n");
+
+    int a=20;
+
+    int b = --a;
+    printf("a = %d\n",a);
+    printf("b = %d\n",b);
+
+
+
+
+    getch();
+}
+
