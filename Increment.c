8099c3d95cc5746eb0e83f061d6d19988ba6a786
sb shuvon <sbshuvon07@gmail.com>
2022-06-14 00:21:02 -0600

    Create Increment.c

---
+++ Increment.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int x=10;
+
+    int y = x++;
+    printf("x = %d\n",x);
+    printf("y = %d\n",y);
+    printf("\n\n");
+
+    int a=20;
+
+    int b = ++a;
+    printf("a = %d\n",a);
+    printf("b = %d\n",b);
+
+
+
+
+    getch();
+}
