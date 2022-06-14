4331fd4c334bbd4f8397dde85e7cb0fa027d5a3d
sb shuvon <sbshuvon07@gmail.com>
2022-06-14 00:20:55 -0600

    Create Unary_Opertor.c

---
+++ Unary_Opertor.c
@@ -0,0 +1,19 @@
+
+// Unary Operator in c-program
+
+#include<stdio.h>
+int main()
+{
+    int x,y,result1,result2;
+
+    x=10;
+    result1= +x;
+    printf("x= %d\n\n",result1);
+
+    y=20;
+    result2= -y;
+    printf("y= %d\n\n",result2);
+
+
+    getch();
+}
