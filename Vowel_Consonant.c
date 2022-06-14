6d363b124106e529d8f2ddc2c237e4c8e5f27067
sb shuvon <sbshuvon07@gmail.com>
2022-06-14 09:58:10 -0600

    Create Vowel_Consonant.c

---
+++ Vowel_Consonant.c
@@ -0,0 +1,20 @@
+// Enter a letter to check Vowel or Consonant
+
+#include<stdio.h>
+int main()
+{
+    char ch;
+
+    printf("Enter any letter: ");
+    scanf("%c",&ch);
+
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ||
+       ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+       printf("Vowel\n");
+    else
+        printf("Consonant\n");
+
+
+
+    getch();
+}
