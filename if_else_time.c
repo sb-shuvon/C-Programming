9af07a19c6e33645056fed1df52a573cc21ad51f
sb shuvon <sbshuvon07@gmail.com>
2022-06-14 01:03:15 -0600

    Create if_else_time.c

---
+++ if_else_time.c
@@ -0,0 +1,21 @@
+// time management using if-else
+
+#include<stdio.h>
+int main()
+{
+    int time=10;
+
+    if(time==10)
+    {
+        printf("Good morning\n");
+        printf("Nilima Tasnim Bappi\n");
+    }
+    else
+    {
+        printf("Sorry it's not morning\n");
+        printf("Nilima Tasnim Bappi\n");
+    }
+
+
+    getch();
+}
