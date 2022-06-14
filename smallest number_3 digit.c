8e071cc932786a06d18adf3d2118e826718ad2cc
sb shuvon <sbshuvon07@gmail.com>
2022-06-14 10:15:10 -0600

    Create smallest number_3 digit.c

---
+++ smallest number_3 digit.c
@@ -0,0 +1,26 @@
+
+//Write a program that read 3 numbers and display minimum
+
+#include<stdio.h>
+int main()
+{
+    int num1,num2,num3;
+
+    printf("Enter 3 numbers: ");
+    scanf("%d %d %d",&num1,&num2,&num3);
+
+    if(num1<num2 && num1<num3)
+        printf("Small number: %d",num1);
+
+    else if(num2<num1 && num2<num3)
+        printf("Small number: %d",num2);
+
+    else if(num3<num1 && num3<num2)
+        printf("Small number: %d",num3);
+
+    else
+        printf("Numbers are equal");
+
+    getch();
+}
+
