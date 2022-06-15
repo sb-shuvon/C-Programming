475ad331ba7bba7c8d85c71fdde8c7017d9f1ba4
sb shuvon <sbshuvon07@gmail.com>
2022-06-14 18:56:33 -0600

    Create Global_Variable.c

---
+++ Global_Variable.c
@@ -0,0 +1,21 @@
+
+//Global Varialble
+
+#include<stdio.h>
+
+int a=10;
+int main()
+{
+    printf("Inside the main function a= %d\n",a);
+    display();
+
+
+    getch();
+}
+
+void display()
+{
+    printf("Inside the display function a= %d\n",a);
+
+
+}
