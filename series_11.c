68eafd3912917d65265fac39aa496dde0563fb2e
sb shuvon <sbshuvon07@gmail.com>
2022-06-18 08:58:02 -0600

    Create series_11.c

---
+++ series_11.c
@@ -0,0 +1,28 @@
+
+// 1 + 1/2 + 1/3 + ...+ 1/n
+
+#include<stdio.h>
+int main()
+{
+    double n,i,sum=0;
+
+    printf("Enter n: ");
+    scanf("%lf",&n);
+
+    for(i=1; i<=n; i++)
+    {
+        sum= sum + (1/i);
+
+        if(i==1)
+            printf("\n1 + ");
+
+        else if(i==n)
+            printf("1/%lf",i);
+        else
+            printf("(1/%lf) + ",i);
+
+    }
+    printf("= %.2lf\n",sum);
+
+    getch();
+}
