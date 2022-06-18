6cc8cf08a629b072a5860038e59cfdecfb9f5e31
sb shuvon <sbshuvon07@gmail.com>
2022-06-18 08:57:29 -0600

    Update 1-100 print _Loop.c

--- 1-100 print _Loop.c
+++ 1-100 print _Loop.c
@@ -1,12 +1,16 @@
 
 //1-100 print using Loop
+//1-n print using Loop
 
 #include<stdio.h>
 int main()
 {
     //for loop
-    int i;
-    for(i=1; i<=100; i++)
+    int i,n;
+
+    printf("Enter n: ");
+    scanf("%d",&n);
+    for(i=2; i<=n; i=i+2)
         printf("%d\n",i);
 
     getch();
