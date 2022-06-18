664971bb21d31386d21d6a9acfe743691e9a6784
sb shuvon <sbshuvon07@gmail.com>
2022-06-18 02:26:26 -0600

    Create series_4.c

---
+++ series_4.c
@@ -0,0 +1,21 @@
+
+// 1 + 2 + 3 + ...... + N
+
+#include<stdio.h>
+int main()
+{
+    int n,i,sum=0;
+
+    printf("Enter n: ");
+    scanf("%d",&n);
+
+    printf("1 + 2 + 3 + ...... + %d ",n);
+
+    for(i=1; i<=n; i++)
+    {
+        sum= sum+i;
+    }
+    printf("= %d ",sum);
+
+    getch();
+}
